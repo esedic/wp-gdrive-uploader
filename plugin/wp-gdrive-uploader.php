268590d6d2e9c19457984328e25a80479a50088c
Elvis Sedić <elvis.sedic@gmail.com>
2025-07-21 21:10:03 +0200

    Load Composer Autoloader

--- plugin/wp-gdrive-uploader.php
+++ plugin/wp-gdrive-uploader.php
@@ -10,6 +10,8 @@ if (!defined('ABSPATH')) exit;
 
 define('WP_GDRIVE_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
+require_once WP_GDRIVE_PLUGIN_PATH . 'vendor/autoload.php';
+
 require_once WP_GDRIVE_PLUGIN_PATH . 'includes/drive-client.php';
 require_once WP_GDRIVE_PLUGIN_PATH . 'includes/admin-page.php';
 require_once WP_GDRIVE_PLUGIN_PATH . 'includes/uploader.php';
