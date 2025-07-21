de6707b7b637c5a6975b0d23b515ebc791816e89
Elvis Sedić <elvis.sedic@gmail.com>
2025-07-21 21:25:48 +0200

    Trigger upload with cron event

--- plugin/wp-gdrive-uploader.php
+++ plugin/wp-gdrive-uploader.php
@@ -17,3 +17,29 @@ require_once WP_GDRIVE_PLUGIN_PATH . 'includes/admin-page.php';
 require_once WP_GDRIVE_PLUGIN_PATH . 'includes/uploader.php';
 
 new WP_GDrive_FileUploader();
+
+
+add_filter('cron_schedules', function($schedules) {
+    $schedules['every_2_minutes'] = [
+        'interval' => 120,
+        'display'  => __('Every 2 Minutes')
+    ];
+    return $schedules;
+});
+
+
+register_activation_hook(__FILE__, function() {
+    if (!wp_next_scheduled('wp_gdrive_upload_cron')) {
+        wp_schedule_event(time(), 'every_2_minutes', 'wp_gdrive_upload_cron');
+    }
+});
+
+register_deactivation_hook(__FILE__, function() {
+    wp_clear_scheduled_hook('wp_gdrive_upload_cron');
+});
+
+add_action('wp_gdrive_upload_cron', function() {
+    WP_GDrive_Uploader::upload_files();
+});
+
+do_action('wp_gdrive_upload_cron');
